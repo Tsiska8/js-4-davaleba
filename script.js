81306e070fe5aedfc4912c9732627d6ed837d510
Ketevan <qeto71@gmail.com>
2023-04-25 21:28:46 +0400

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+function colorFunction() {
+    var x = document.getElementById("color1").style.backgroundColor = "blue";
+     
+    var y = document.getElementById("color2").style.backgroundColor = "green";
+}
