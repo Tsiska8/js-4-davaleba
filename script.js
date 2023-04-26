843331c7394e973205abbb7ff5e731a02af5b8f5
Ketevan <qeto71@gmail.com>
2023-04-26 21:13:04 +0400

    update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function colorFunction() {
-    var x = document.getElementById("color1").style.backgroundColor = "blue";
+    var x =  document.body.style.backgroundColor = prompt('background color?', 'blue');
+
+    var y =  document.body.style.backgroundColor = prompt('background color?', 'green');
+    
+    //var x = document.getElementById("color1").style.backgroundColor = "blue";
      
-    var y = document.getElementById("color2").style.backgroundColor = "green";
+    //var y = document.getElementById("color2").style.backgroundColor = "green";
 }
 
